007278ab289e90e88f05b2ed3c0442d7ff482b5f
Jitendra Kumar Sharma <jitendra@quikieapps.com>
2020-11-20 16:40:00 +0530

    ckd removed

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 app = Flask('Heart Disease')
 CORS(app)
 model = pickle.load(open('heart_model.pkl','rb'))
-ckd_model = keras.models.load_model('ckd.model')
+# ckd_model = keras.models.load_model('ckd.model')
 @app.route('/')
 def home():
     return send_from_directory('build',"index.html")
@@ -29,15 +29,15 @@ def heart():
     result = int(model.predict(inputData)[0])
     return jsonify(result=result)
 
-@app.route('/predict/ckd' ,methods=['POST'])
-def kidney():
-    data = request.get_json(force=True)
-    arrData = list(data.values())
-    del arrData[-1]
-    print([arrData])
-    result = ckd_model.predict([arrData])
-    print(result[0][0])
-    return jsonify(result=float(result[0][0]))
+# @app.route('/predict/ckd' ,methods=['POST'])
+# def kidney():
+#     data = request.get_json(force=True)
+#     arrData = list(data.values())
+#     del arrData[-1]
+#     print([arrData])
+#     result = ckd_model.predict([arrData])
+#     print(result[0][0])
+#     return jsonify(result=float(result[0][0]))
 
 if __name__ == '__main__':
     app.run(debug=True , host='0.0.0.0',port=8080)
