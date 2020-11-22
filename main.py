ffc62864b8a9a7f6ac80672c9013ba73f4402150
Jitendra Kumar Sharma <jitendra@quikieapps.com>
2020-11-23 00:53:40 +0530

    updates

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import numpy as np
 from flask import Flask,jsonify,request,render_template,send_from_directory,make_response
 import pickle
 from flask_cors import CORS, cross_origin
-import keras
-import keras as k
+# from tensorflow import keras
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
@@ -11,7 +10,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 app = Flask('Heart Disease')
 CORS(app)
 model = pickle.load(open('heart_model.pkl','rb'))
-ckd_model = k.models.load_model('ckd.model')
+# ckd_model = keras.models.load_model('ckd.model')
 @app.route('/')
 def home():
     return send_from_directory('build',"index.html")
@@ -30,15 +29,15 @@ def heart():
     result = int(model.predict(inputData)[0])
     return jsonify(result=result)
 
-@app.route('/predict/ckd' ,methods=['POST'])
-def kidney():
-    data = request.get_json(force=True)
-    arrData = list(data.values())
-    del arrData[-1]
-    print([arrData])
-    result = ckd_model.predict([arrData])
-    print(result[0][0])
-    return jsonify(result=float(result[0][0]))
+# @app.route('/predict/ckd' ,methods=['POST'])
+# def kidney():
+#     data = request.get_json(force=True)
+#     arrData = list(data.values())
+#     del arrData[-1]
+#     print([arrData])
+#     result = ckd_model.predict([arrData])
+#     print(result[0][0])
+#     return jsonify(result=float(result[0][0]))
 
 if __name__ == '__main__':
     app.run(debug=True , host='0.0.0.0',port=8080)
